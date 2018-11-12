5e6c8d450f8184c1837ba9ebf702400acd6cc9f3
liuzhe <zhethink@gmail.com>
2018-11-12 12:00:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+
+//第二次
+const upload = multer({dest: 'uploads/'});
+
+//加入cors
+const cors = require('cors');
+
+
+const app = express();
+
+app.get('/', (req, res)=>{
+  res.send('hello liuZhe!');
+});
+
+app.post('/upload', cors(), upload.single('file'), (req, res) =>{
+  // console.log(req.file.filename);
+  res.send(req.file.filename);
+});
+
+app.get('/preview/:key',cors(), (req, res) =>{
+  res.sendfile(
+    `uploads/${req.params.key}`,
+    {
+      root: __dirname,
+      headers: {
+        'Content-Type': 'image/jpeg'
+      }
+    }
+  );
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`开启服务成功并且监听在${port}端口!!`);
